ae0c529bfbcfb9cf6aa582785501553916f5e3dd
Sachin kumar prasad <skpkorba9009@gmail.com>
2023-04-06 23:06:46 +0530

    added port

--- src/main.ts
+++ src/main.ts
@@ -10,7 +10,7 @@ async function bootstrap() {
       whitelist: true,
     }),
   );
-  const PORT = process.env.PORT;
+  const PORT = process.env.PORT || 5001;
   await app.listen(PORT);
 }
 bootstrap();
